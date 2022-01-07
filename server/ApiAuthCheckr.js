5428c22dbae74b3547984597dc160449d0fa35d0
Jack Ziesing <jziesing@gmail.com>
2022-01-06 20:25:40 -0800

    try fix auth

--- server/ApiAuthCheckr.js
+++ server/ApiAuthCheckr.js
@@ -8,16 +8,17 @@ module.exports = function(req, res, next) {
 	// protected routes
 	//let stokentype = req.headers['Authorization'];
 	console.log('stokentypesss : ');
-	console.log(req.headers.authorization.substring(5, req.headers.authorization.length));
 	let buff = Buffer.from(req.headers.authorization.substring(5, req.headers.authorization.length), 'base64');
 	let text = buff.toString('ascii');
 	console.log('DEccoooodeddd : ');
-	console.log(text);
-	next();
-  	// if(stokentype) {
-	// 	console.log('Public Stoke Route type : ' + stokentype);
-	// 	next();
-	// } else {
-	// 	res.status(404).end();
-	// }
+	let creds = text.split(':');
+	console.log('creds::::');
+	console.log(creds[0]);
+	console.log(creds[1]);
+	if(creds[0] == process.env.sfc_uname && creds[1] == process.env.sfc_pword) {
+		console.log('ACCESS GRANTED ');
+		next();
+	} else {
+		res.status(404).end();
+	}
 };
