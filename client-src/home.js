7d4943f6fcd4a51cbb7f9a04b03db70e345665a0
Jack Ziesing <jack.ziesing@jziesing-ltm5.internal.salesforce.com>
2022-01-06 23:12:18 -0800

    cleanup

--- client-src/home.js
+++ client-src/home.js
@@ -4,6 +4,9 @@ const Home = () => (
   <div>
     <h1>Home</h1>
     <strong>Home page</strong>
+    <p>This demo uses Heroku Connect, Salesforce Connect, and a small bit of custom code to archive salesforce data on Heroku Postgres, but still have read access to it using Salesforce Connect.</p>
+    <p>There is custom code running on Heroku that is a small API that takes authenicated requests to archive a Salesforce Case.  When it archives a case, it simple creates a copy of the case in a table that is not mapped with Heroku Connect.</p>
+    <p>There is also custom code running on Salesforce that is a trigger whenever a Case record is deleted.  When Case's are deleted, an archived copy is stored in Heroku Postgres that is viewable in Salesforce using OData and Salesforce Connect, but doesn't eat at Salesforce storage limits.</p>
   </div>
 );
 
