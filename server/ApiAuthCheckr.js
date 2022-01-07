d5abdc3d54b497d08100768c6e6b671f0f85eba9
Jack Ziesing <jziesing@gmail.com>
2022-01-06 20:20:07 -0800

    try fix auth

--- server/ApiAuthCheckr.js
+++ server/ApiAuthCheckr.js
@@ -8,8 +8,8 @@ module.exports = function(req, res, next) {
 	// protected routes
 	//let stokentype = req.headers['Authorization'];
 	console.log('stokentypesss : ');
-	console.log(req.headers.authorization);
-	let buff = Buffer.from(req.headers.authorization, 'base64');
+	console.log(req.headers.authorization.substring(5, req.headers.authorization.length));
+	let buff = Buffer.from(req.headers.authorization.substring(5, req.headers.authorization.length), 'base64');
 	let text = buff.toString('ascii');
 	console.log('DEccoooodeddd : ');
 	console.log(text);
