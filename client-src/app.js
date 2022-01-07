99333ef22fb31217856a235fdb10724f51b85fc3
Jack Ziesing <jziesing@gmail.com>
2022-01-07 11:13:28 -0800

    UI fixes

--- client-src/app.js
+++ client-src/app.js
@@ -6,6 +6,7 @@ import {
   Link
 } from 'react-router-dom';
 import Home from './home';
+import Demovid from './demovid';
 
 const App = () => (
   <Router>
@@ -13,10 +14,13 @@ const App = () => (
       <header>
         <Link to="/">Home</Link>
         {' '}
+        <Link to="/demo">Demo Video</Link>
+        {' '}
       </header>
       <main>
         <Switch>
           <Route exact path="/" component={Home} />
+		  <Route exact path="/demo" component={Demovid} />
         </Switch>
       </main>
     </div>
---
+++ client-src/demovid.js
@@ -0,0 +1,7 @@
+const Demovid = () => (
+  	<div>
+	  <span>ADD DEMO VIDEO!</span>
+	</div>
+);
+
+export default Demovid;
